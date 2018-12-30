35f637043994ec6a8bd77b95eedd15e37200f5f0
Tony Tang <tonytang215@hotmail.com>
2018-12-30 19:18:46 +1100

    Added README.md, .gitignore and minor landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
+import "./App.css";
+import Grid from "@material-ui/core/Grid";
 
 class App extends Component {
   render() {
-    return (
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    );
+    return <div className="landing">Tony Tang</div>;
   }
 }
 
