295a472aff5f9ef27fa43fa9f0ad84c4a5b3a1fd
Tony Tang <tonytang215@hotmail.com>
2019-01-06 22:36:36 +1100

    design change + landing.js and css cleanup refactor

--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -1,52 +1,48 @@
 import React, { Component } from "react";
 // import GithubIcon from "../public/images/github-icon.svg";
 import SVG from "react-inlinesvg";
-import Icon from "@material-ui/core/Icon";
+// import Icon from "@material-ui/core/Icon";
 import GithubIcon from "./icons/github-icon.svg";
 import LinkedInIcon from "./icons/linkedin-icon.svg";
 
 class Landing extends Component {
   render() {
     return (
-      <div className="landing">
-        <div className="landing-main-container">
-          <div className="landing-greeting">
-            <span>Hi, I'm</span>
-          </div>
-          <div className="landing-content">
-            <div className="landing-name-box">
-              <div className="landing-name">Tony Tang</div>
+      <header id="landing">
+        <div id="main-container">
+          <div id="greeting">Hi, I'm</div>
+          <div id="banner">
+            <div id="name">Tony Tang</div>
+            <div id="statement">
+              <div>Sydney</div>
+              <div id="vertical-separator" />
+              <div id="roles">
+                <div className="role">Software Engineer</div>
+                <div className="role">Designer</div>
+                <div className="role">Leader</div>
+              </div>
             </div>
-            <div className="landing-details">
-              <div id="landing-statement">
-                Sydney based
-                <div id="vertical-separator" />
-                <div className="landing-details-roles">
-                  <div className="landing-details-role">Software Engineer</div>
-                  <div className="landing-details-role">Designer</div>
-                  <div className="landing-details-role">Leader</div>
-                </div>
-              </div>
-              <div id="landing-links">
-                <a
-                  href="https://github.com/Mysterise"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <SVG src={GithubIcon} className="svg-link" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/tonyztang/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <SVG src={LinkedInIcon} className="svg-link" />
-                </a>
-              </div>
+          </div>
+          <div id="under-banner">
+            <div id="links">
+              <a
+                href="https://github.com/Mysterise"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SVG src={GithubIcon} className="svg-link" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/tonyztang/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SVG src={LinkedInIcon} className="svg-link" />
+              </a>
             </div>
           </div>
         </div>
-      </div>
+      </header>
     );
   }
 }
