59669377616b985b05dd61a957832be8331352b3
Tony Tang <tonytang.dev@gmail.com>
2020-03-08 04:26:47 +1100

    Server serves client now!

--- server/app.ts
+++ server/app.ts
@@ -4,8 +4,7 @@ import path from 'path';
 const app = express();
 const port = 8000;
 
-app.use('/static', express.static(path.join(__dirname, '../client/build')));
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/build', 'index.html')));
-app.get('/', (req, res) => res.send('hello'));
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/build', 'index.html')));
 
-app.listen(port, () => console.log('tonytang.io started.'));
+app.listen(port, () => console.log(`tonytang.io listening on port ${port}`));
