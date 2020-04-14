167e258af0183ae095d87bd210a6a52bc43e73fa
Tony Tang <tonytang.dev@gmail.com>
2020-04-15 02:59:19 +1000

    Introduced custom.d.ts to fix express type error

--- server/dist/server.js
+++ server/dist/server.js
@@ -13,6 +13,8 @@ const signup_1 = __importDefault(require("./api/signup"));
 const login_1 = __importDefault(require("./api/login"));
 const authJwt_1 = require("./authJwt");
 const secret_1 = __importDefault(require("./api/secret"));
+const getBlog_1 = __importDefault(require("./api/getBlog"));
+const makeBlog_1 = __importDefault(require("./api/makeBlog"));
 const { port } = config_1.default;
 const server = express_1.default();
 mongoose_1.default.connect(connections_1.default.mongoDb, {
@@ -26,6 +28,8 @@ server.use(body_parser_1.default.json());
 server.use(express_1.default.static(path_1.default.join(__dirname, '../../client/build')));
 server.get('/', (req, res) => res.sendFile(path_1.default.join(__dirname, '../client/build', 'index.html')));
 server.get('/api/secret', authJwt_1.verifyToken, secret_1.default);
+server.get('/api/getBlog', getBlog_1.default);
+server.post('/api/makeBlog', makeBlog_1.default);
 server.post('/api/signup', signup_1.default);
 server.post('/api/login', login_1.default);
 server.listen(port, () => console.log(`tonytang.io listening on port ${port}`));
